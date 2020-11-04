c489b37293d214a7b12a757fc8def424ed9f595d
Departuers <2644631299@qq.com>
2020-11-04 21:44:43 +0800

    c

---
+++ 整体二分/修改区间第k小.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+using namespace std;
+const int inf = 0x3f3f3f3f;
+int n, m;
+struct node
+{
+    int op;//操作类型1是修改,2是查询
+    int x,y,k;
+    int id;
+};
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
